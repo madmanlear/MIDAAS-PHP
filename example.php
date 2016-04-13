030ab6f822cc51340f7c73a2b42f1fdb755553eb
Chris Davis <madmanlear@gmail.com>
2016-04-13 18:24:16 -0400

    simple example

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$midaas = new Madmanlear\Midaas;
+
+$fields = [
+  'sex'   => 'female',
+  'state' => 'GA',
+];
+
+$results = $midaas->incomeDistribution($fields, true);
+
+print_r($results);
